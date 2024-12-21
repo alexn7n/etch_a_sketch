89a193326f305ca599ef7459032d556afb0131e7
alexn7n <156884962+alexn7n@users.noreply.github.com>
2024-12-22 10:07:00 +1100

    add function to generate squares

--- events.js
+++ events.js
@@ -1,15 +1,30 @@
 const containerElement = document.querySelector("#container")
 const squareElement = document.createElement("div")
 squareElement.setAttribute("id", "square");
-const NUMBER_OF_SQUARES = 16*16
+let size = 16
+let flexBasis = `${100 / size}%`;
+let canvasSize = size*size
+
+const sizeButtonElement = document.querySelector("#size-button")
 
 function createCanvas() {
-for (let i = 0; i < NUMBER_OF_SQUARES; i++) {
+for (let i = 0; i < canvasSize; i++) {
     containerElement.appendChild(squareElement.cloneNode(true));
+    squareElement.style.flexBasis = flexBasis;
   }
 }
 
 window.onload = function() {
-    createCanvas(NUMBER_OF_SQUARES, containerElement, squareElement);
+    createCanvas();
   };
+
+sizeButtonElement.addEventListener('click', handleClickEvent);
+
+function handleClickEvent() {
+    let size = prompt("Which size would you like");
+    if (size < 100) {
+    canvasSize = size*size
+    createCanvas();
+    } else alert("enter less than 100");
+}
 
